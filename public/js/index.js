e671834912ed1f3d340e70fe8c15c7d83d2288da
unknown <kylestauch99@gmail.com>
2018-04-20 13:18:42 -0500

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
     console.log('Connected to server');
-
-    socket.emit('createMessage', {
-        from: 'jen@example.com',
-        text: 'Hey Kyle'
-    });
 });
 
 socket.on('disconnect', function () {
--- server/server.js
+++ server/server.js
@@ -14,14 +14,13 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('User connected');
 
-    socket.emit('newMessage', {
-        from: 'Bill',
-        text: 'Hi',
-        createdAt: 123
-    });
-
     socket.on('createMessage', (message) => {
         console.log(message);
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        });
     });
 
     socket.on('disconnect', () => {
